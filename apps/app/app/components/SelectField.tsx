f13c10eba0cb5711e049b4274f981a169a60835d
Anmol <ainsa2279@gmail.com>
2025-10-04 23:43:52 +0530

    Add: APP - SelectField & FormSelectField for fors

--- apps/app/app/components/SelectField.tsx
+++ apps/app/app/components/SelectField.tsx
@@ -1,32 +1,161 @@
-import React, { forwardRef, type Ref, useImperativeHandle } from "react";
-import { TouchableOpacity, View } from "react-native";
+import {
+	BottomSheetBackdrop,
+	BottomSheetFlatList,
+	BottomSheetFooter,
+	BottomSheetModal,
+} from "@gorhom/bottom-sheet";
+import type { ThemedStyle } from "app/theme";
+import { useAppTheme } from "app/utils/useAppTheme";
+import { forwardRef, type Ref, useImperativeHandle, useRef } from "react";
+import { TouchableOpacity, View, type ViewStyle } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Button } from "./Button";
+import { Icon } from "./Icon";
+import { ListItem } from "./ListItem";
 import { TextField, type TextFieldProps } from "./TextField";
 
 export interface SelectFieldProps
-	extends Omit<
-		TextFieldProps,
-		"ref" | "onValueChange" | "onChange" | "value"
-	> {}
-export type SelectFieldRef = {};
+	extends Omit<TextFieldProps, "ref" | "onValueChange" | "onChange" | "value"> {
+	value?: string[];
+	renderValue?: (value: string[]) => string;
+	onSelect?: (newValue: string[]) => void;
+	multiple?: boolean;
+	options: { label: string; value: string }[];
+}
+export interface SelectFieldRef {
+	presentOptions: () => void;
+	dismissOptions: () => void;
+}
+
+function without<T>(array: T[], value: T) {
+	return array.filter((v) => v !== value);
+}
 
 export const SelectField = forwardRef(function SelectField(
 	props: SelectFieldProps,
 	ref: Ref<SelectFieldRef>,
 ) {
-	const { ...TextFieldProps } = props;
+	const {
+		value = [],
+		onSelect,
+		renderValue,
+		options = [],
+		multiple = true,
+		...TextFieldProps
+	} = props;
+	const sheet = useRef<BottomSheetModal>(null);
+	const { bottom } = useSafeAreaInsets();
+	const {
+		themed,
+		theme: { colors },
+	} = useAppTheme();
 
 	const disabled =
 		TextFieldProps.editable === false || TextFieldProps.status === "disabled";
 
-	useImperativeHandle(ref, () => ({}));
+	useImperativeHandle(ref, () => ({ presentOptions, dismissOptions }));
+
+	const valueString =
+		renderValue?.(value) ??
+		value
+			.map((v) => options.find((o) => o.value === v)?.label)
+			.filter(Boolean)
+			.join(", ");
+
+	function presentOptions() {
+		if (disabled) return;
+
+		sheet.current?.present();
+	}
+
+	function dismissOptions() {
+		sheet.current?.dismiss();
+	}
+
+	function updateValue(optionValue: string) {
+		if (value.includes(optionValue)) {
+			onSelect?.(multiple ? without(value, optionValue) : []);
+		} else {
+			onSelect?.(multiple ? [...value, optionValue] : [optionValue]);
+			if (!multiple) dismissOptions();
+		}
+	}
+
+	const {
+		theme: { spacing },
+	} = useAppTheme();
 
 	return (
 		<>
-			<TouchableOpacity activeOpacity={1}>
+			<TouchableOpacity activeOpacity={1} onPress={presentOptions}>
 				<View pointerEvents="none">
-					<TextField {...TextFieldProps} />
+					<TextField
+						{...TextFieldProps}
+						value={valueString}
+						RightAccessory={(props) => (
+							<Icon icon="caretRight" containerStyle={props.style} />
+						)}
+					/>
 				</View>
 			</TouchableOpacity>
+
+			<BottomSheetModal
+				ref={sheet}
+				snapPoints={["50%"]}
+				stackBehavior="replace"
+				enableDismissOnClose
+				backdropComponent={(props) => (
+					<BottomSheetBackdrop
+						{...props}
+						appearsOnIndex={0}
+						disappearsOnIndex={-1}
+					/>
+				)}
+				footerComponent={
+					!multiple
+						? undefined
+						: (props) => (
+								<BottomSheetFooter
+									{...props}
+									style={themed($bottomSheetFooter)}
+									bottomInset={bottom}
+								>
+									<Button
+										text="Dismiss"
+										preset="reversed"
+										onPress={dismissOptions}
+									/>
+								</BottomSheetFooter>
+							)
+				}
+			>
+				<BottomSheetFlatList
+					style={{
+						marginBottom: bottom + (multiple ? spacing.xl * 2 : 0),
+					}}
+					data={options}
+					keyExtractor={(o) => o.value}
+					renderItem={({ item, index }) => (
+						<ListItem
+							text={item.label}
+							topSeparator={index !== 0}
+							style={themed($listItem)}
+							rightIcon={value.includes(item.value) ? "check" : undefined}
+							rightIconColor={colors.palette.angry500}
+							onPress={() => updateValue(item.value)}
+						/>
+					)}
+				/>
+			</BottomSheetModal>
 		</>
 	);
+});
+
+const $bottomSheetFooter: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+	paddingHorizontal: spacing.lg,
+	paddingBottom: spacing.xs,
+});
+
+const $listItem: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+	paddingHorizontal: spacing.lg,
 });
---
+++ apps/app/app/components/form/FormSelectField.tsx
@@ -0,0 +1,85 @@
+import { Controller, useFormContext } from "react-hook-form";
+import { View, type ViewStyle } from "react-native";
+import { spacing, type ThemedStyle } from "@/theme";
+import { useAppTheme } from "@/utils/useAppTheme";
+import { SelectField, type SelectFieldProps } from "../SelectField";
+
+type FormFieldProps = Omit<
+	SelectFieldProps,
+	"value" | "onChangeText" | "onBlur"
+> & {
+	name: string;
+};
+
+export const FormSelectField = (props: FormFieldProps) => {
+	const { control, formState } = useFormContext();
+	const { themed } = useAppTheme();
+
+	type T = typeof formState.errors;
+
+	const getValue = (obj: T, path: string) =>
+		path.split(".").reduce((acc, key) => acc && acc[key], obj);
+
+	const error = getValue(formState?.errors, props.name)?.message.toString();
+
+	return (
+		<View>
+			<Controller
+				control={control}
+				render={({ field: { onChange, onBlur, value, disabled } }) => (
+					<SelectField
+						value={value}
+						renderValue={(value) => {
+							if (props.multiple) {
+								return value
+									.map((v) => props.options.find((o) => o.value === v)?.label)
+									.filter(Boolean)
+									.join(", ");
+							}
+							const foundOption = props.options.find(
+								(option) => option.value === value,
+							);
+
+							return foundOption?.label;
+						}}
+						onSelect={(v) => {
+							console.log("onSelect");
+							if (props.multiple) {
+								if (Array.isArray(v)) {
+									onChange(v);
+								} else {
+									onChange([v]);
+								}
+							} else {
+								if (Array.isArray(v)) {
+									onChange(v[0]);
+								} else {
+									onChange(v);
+								}
+							}
+						}}
+						onChangeText={(val) => {
+							console.log("onChangeText");
+							console.log(val);
+							onChange(val);
+						}}
+						onBlur={onBlur}
+						containerStyle={themed($textField)}
+						status={disabled ? "disabled" : error ? "error" : undefined}
+						helper={props.helper || error}
+						{...props}
+					/>
+				)}
+				name={props.name}
+			/>
+			{/*
+      <Text preset="error" >{error}</Text>
+      */}
+		</View>
+	);
+};
+
+const $textField: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+	marginBottom: spacing.lg,
+	borderRadius: spacing.xxl,
+});
