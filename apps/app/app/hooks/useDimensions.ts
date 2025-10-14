9071a4e58005517832133b34f55a28cdaea9393c
Anmol <ainsa2279@gmail.com>
2025-10-14 23:24:36 +0530

    perf: APP - `useDimensions` hook performance

--- apps/app/app/hooks/useDimensions.ts
+++ apps/app/app/hooks/useDimensions.ts
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { Dimensions } from "react-native";
 
 export const useDimensions = () => {
-	const [dimensions, setDimensions] = useState({
+	const [dimensions, setDimensions] = useState(() => ({
 		width: Dimensions.get("window").width,
 		height: Dimensions.get("window").height,
-	});
+	}));
 
 	useEffect(() => {
 		const subscription = Dimensions.addEventListener("change", ({ window }) => {
