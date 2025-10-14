69722cb81c527e2a8a8e32d7d25a9bf7ef4c422f
Anmol <ainsa2279@gmail.com>
2025-10-14 23:19:29 +0530

    Fix: APP - Remove API URL added for debugging

--- apps/app/app/modules/home/screens/HomeScreen.tsx
+++ apps/app/app/modules/home/screens/HomeScreen.tsx
@@ -1,13 +1,12 @@
 import { QuickActions } from "@home/components";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { Screen, Text } from "@/components";
+import { Screen } from "@/components";
 import { getCalculatorsOpts } from "@/modules/Calculator/hooks/queries";
 import { BackToLessonCard } from "@/modules/lesson/components/BackToLessonCard";
 import { getLessonsOpts } from "@/modules/lesson/hooks/api";
 import { getScamsOpts } from "@/modules/scam/hooks/queries";
 import { $styles, spacing } from "@/theme";
-import { envs } from "@/utils/envs";
 
 export function HomeScreen() {
 	const queryClient = useQueryClient();
@@ -20,12 +19,11 @@ export function HomeScreen() {
 
 	return (
 		<Screen
-			preset="fixed"
+			preset="scroll"
 			contentContainerStyle={[$styles.container, { gap: spacing.xs }]}
 			safeAreaEdges={["top", "bottom"]}
 		>
 			<BackToLessonCard />
-			<Text>{envs.API_URL}</Text>
 			<QuickActions />
 		</Screen>
 	);
