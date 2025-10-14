8781f47b137335ddc47c7bf5be0c0e7c08f62323
Anmol <ainsa2279@gmail.com>
2025-10-14 23:23:59 +0530

    Add: APP - `useIsOnline` hook

---
+++ apps/app/app/hooks/useIsOnline.ts
@@ -0,0 +1,16 @@
+import NetInfo from "@react-native-community/netinfo";
+import { useEffect, useState } from "react";
+
+export const useIsOnline = () => {
+	const [isConnected, setIsConnected] = useState(true);
+
+	useEffect(() => {
+		const unsubscribe = NetInfo.addEventListener((state) => {
+			setIsConnected(state.isConnected ?? false);
+		});
+
+		return unsubscribe;
+	}, []);
+
+	return isConnected;
+};
