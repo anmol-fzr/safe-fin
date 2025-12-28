5c355e58bd706a258bea18be7c802d5828d87714
Anmol <ainsa2279@gmail.com>
2025-12-28 15:45:44 +0530

    Fix: [APP] - Using Screen from Modules

--- apps/app/app/routes/tabs/index.tsx
+++ apps/app/app/routes/tabs/index.tsx
@@ -1,30 +1,3 @@
-import { useQueryClient } from "@tanstack/react-query";
-import { useEffect } from "react";
-import { Screen } from "@/components";
-import { getCalculatorsOpts } from "@/modules/calculator/hooks/queries";
-import { QuickActions } from "@/modules/home/components/quick-actions";
-import { BackToLessonCard } from "@/modules/lesson/components/BackToLessonCard";
-import { getLessonsOpts } from "@/modules/lesson/hooks/api";
-import { getScamsOpts } from "@/modules/scam/hooks/queries";
-import { $styles, spacing } from "@/theme";
+import { HomeScreen } from "@/modules/home/screens";
 
-export default function HomeScreen() {
-	const queryClient = useQueryClient();
-
-	useEffect(() => {
-		queryClient.prefetchInfiniteQuery(getCalculatorsOpts());
-		queryClient.prefetchInfiniteQuery(getScamsOpts());
-		queryClient.prefetchInfiniteQuery(getLessonsOpts());
-	}, [queryClient.prefetchInfiniteQuery]);
-
-	return (
-		<Screen
-			preset="scroll"
-			contentContainerStyle={[$styles.container, { gap: spacing.xs }]}
-			safeAreaEdges={["top", "bottom"]}
-		>
-			<BackToLessonCard />
-			<QuickActions />
-		</Screen>
-	);
-}
+export default HomeScreen;
