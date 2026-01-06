9ed2477b904d3fffef8b5739067050d20780c247
Anmol <ainsa2279@gmail.com>
2026-01-06 13:14:48 +0530

    Refactor: [UI] make use utils pkg

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,4 +1,3 @@
 const getEmptyArr = (length = 10) => Array.from({ length }, (_, i) => i);
 
 export { getEmptyArr };
-export * from "./type-utils";
--- packages/ui/src/utils/type-utils.ts
+++
@@ -1,9 +0,0 @@
-function isUndefined(val: unknown): val is undefined {
-	return val === undefined;
-}
-
-function isNull(val: unknown): val is null {
-	return val === null;
-}
-
-export { isUndefined, isNull };
