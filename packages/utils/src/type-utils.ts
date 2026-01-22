7aeeded2d99c83648619827aefa4a587a9782c50
Anmol <macmol@Anmols-MacBook-Air.local>
2026-01-22 19:21:06 +0530

    Add: [UTILS] - isNotNull func

--- packages/utils/src/type-utils.ts
+++ packages/utils/src/type-utils.ts
@@ -5,3 +5,7 @@ export function isUndefined(val: unknown): val is undefined {
 export function isNull(val: unknown): val is null {
 	return val === null;
 }
+
+export function isNotNull<T>(val: T | null | undefined): val is NonNullable<T> {
+	return val !== null && val !== undefined;
+}
