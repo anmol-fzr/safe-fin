4b30b62774971e6934ffc65e4333467e58609c9c
Anmol <macmol@Anmols-MacBook-Air.local>
2026-01-22 14:02:48 +0530

    Add: Profile and Address Router to v1 Router

--- apps/server/src/api/v1/router.ts
+++ apps/server/src/api/v1/router.ts
@@ -1,13 +1,13 @@
 import { createTypedFactory } from "@/factory";
 import { authenticate, db } from "@/middleware";
-//import { addressRouter } from "@/modules/address";
+import { addressRouter } from "@/modules/address";
 import { authRouter } from "@/modules/auth";
 import { calculatorRouter } from "@/modules/calculator";
 import { lessonRouter } from "@/modules/lesson";
 import { savedRouter } from "@/modules/saved";
 import storageRouter from "@/modules/storage/storage.controller";
 
-//import { profileRouter } from "@/modules/profile";
+import { profileRouter } from "@/modules/profile";
 //import { quizRouter } from "@/modules/quiz";
 
 const { createApp } = createTypedFactory();
@@ -20,9 +20,8 @@ v1Router
 	.route("/courses", lessonRouter)
 	.route("/calculator", calculatorRouter)
 	.route("/saved", savedRouter)
-	.route("/storage", storageRouter);
-
-//.route("/address", addressRouter)
-//.route("/profile", profileRouter);
+	.route("/storage", storageRouter)
+	.route("/profile", profileRouter)
+	.route("/address", addressRouter);
 
 export { v1Router };
