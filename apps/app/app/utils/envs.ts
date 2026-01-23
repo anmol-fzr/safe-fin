a6d0c60d7e11661c20b0b8cdaa2b1707055d410c
Anmol <macmol@Anmols-MacBook-Air.local>
2026-01-23 14:09:06 +0530

    Add: [APP] - Defautls to envs

--- apps/app/app/utils/envs.ts
+++ apps/app/app/utils/envs.ts
@@ -5,21 +5,30 @@ const isDev = env.EXPO_PUBLIC_MODE === "DEV" || __DEV__;
 
 export const envs = Object.freeze({
 	isDev,
-	API_URL: env.EXPO_PUBLIC_API_URL ?? "http://192.168.29.57:3000/api/v1",
+	API_URL: "https://safe-fin.anmol-fzr.workers.dev/api/v1",
 	get AUTH_API_URL() {
 		return `${this.API_URL}/auth`;
 	},
 	SENTRY: Object.freeze({
-		DSN: env.EXPO_PUBLIC_SENTRY_DSN,
+		DSN:
+			env.EXPO_PUBLIC_SENTRY_DSN ??
+			"https://ba79f0ccb7a703cd14e3bd6416535916@o4506835291078656.ingest.us.sentry.io/4506835294748672",
 	}),
 	META_URLS: Object.freeze({
-		PLAYSTORE: env.EXPO_PUBLIC_METADATA_PLAYSTORE as ExternalPathString,
-		APPSTORE: env.EXPO_PUBLIC_METADATA_APPSTORE as ExternalPathString,
-		GITHUB: env.EXPO_PUBLIC_METADATA_GITHUB as ExternalPathString,
+		PLAYSTORE: (env.EXPO_PUBLIC_METADATA_PLAYSTORE ??
+			"https://github.com/anmol-fzr/safe-fin") as ExternalPathString,
+		APPSTORE: (env.EXPO_PUBLIC_METADATA_APPSTORE ??
+			"https://github.com/anmol-fzr/safe-fin") as ExternalPathString,
+		GITHUB: (env.EXPO_PUBLIC_METADATA_GITHUB ??
+			"https://github.com/anmol-fzr/safe-fin") as ExternalPathString,
 
-		ABOUT: env.EXPO_PUBLIC_METADATA_ABOUT_URL as ExternalPathString,
-		TERMS: env.EXPO_PUBLIC_METADATA_TERMS_URL as ExternalPathString,
-		SUPPORT: env.EXPO_PUBLIC_METADATA_SUPPORT_URL as ExternalPathString,
-		POLICY: env.EXPO_PUBLIC_METADATA_POLICY_URL as ExternalPathString,
+		ABOUT: (env.EXPO_PUBLIC_METADATA_ABOUT_URL ??
+			"https://safefin.framer.website/#about") as ExternalPathString,
+		TERMS: (env.EXPO_PUBLIC_METADATA_TERMS_URL ??
+			"https://safefin.framer.website/terms-conditions") as ExternalPathString,
+		SUPPORT: (env.EXPO_PUBLIC_METADATA_SUPPORT_URL ??
+			"https://safefin.framer.website/") as ExternalPathString,
+		POLICY: (env.EXPO_PUBLIC_METADATA_POLICY_URL ??
+			"https://safefin.framer.website/privacy-policy") as ExternalPathString,
 	}),
 });
