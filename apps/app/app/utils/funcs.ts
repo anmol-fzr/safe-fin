d311ec2580d877c349186a69220e960f93202fad
Anmol <macmol@Anmols-MacBook-Air.local>
2026-01-23 14:08:54 +0530

    Remove: [APP] - temp and Unused code

--- apps/app/app/utils/funcs.ts
+++ apps/app/app/utils/funcs.ts
@@ -4,25 +4,6 @@ const currenctFmt = new Intl.NumberFormat("en-IN", {
 	maximumFractionDigits: 0,
 });
 
-function isStrictlySameObj(
-	checkFromObj: Record<string, unknown>,
-	checkAtObj: Record<string, unknown>,
-) {
-	let isSame = true;
-
-	for (const [key, value] of Object.entries(checkFromObj)) {
-		const valInOther = checkAtObj[key];
-		console.log(`Checking, ${value} against ${valInOther}`);
-
-		if (valInOther === undefined || valInOther !== value) {
-			isSame = false;
-			break;
-		}
-	}
-
-	return isSame;
-}
-
 type DebouncingFunc<T> = (args: T) => void;
 
 export function debounce<A>(func: DebouncingFunc<A>, timeout = 300) {
@@ -39,4 +20,4 @@ export function debounce<A>(func: DebouncingFunc<A>, timeout = 300) {
 	return fn;
 }
 
-export { currenctFmt, isStrictlySameObj };
+export { currenctFmt };
