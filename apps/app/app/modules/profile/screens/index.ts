d2313ab6468e58fef278a69c02f17f0ddc5d4ea5
Anmol <macmol@Anmols-MacBook-Air.local>
2026-01-24 19:34:04 +0530

    Removed: [APP] - Unused Profile Screen and Components

--- apps/app/app/modules/profile/screens/index.ts
+++ apps/app/app/modules/profile/screens/index.ts
@@ -1,5 +1,4 @@
 export * from "./AccountScreen";
 export * from "./DemoGraphicsScreen";
 export * from "./FinancialDetailsScreen";
-export * from "./ProfileIndexScreen";
 export * from "./UserProfileScreen";
