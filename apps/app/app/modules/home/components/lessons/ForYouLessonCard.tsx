b2dbbca0008990a44437dcecf46c087232b747be
Anmol <macmol@Anmols-MacBook-Air.local>
2026-01-24 18:10:47 +0530

    Refactor: [APP] - ForYouLesson in Home Screen

---
+++ apps/app/app/modules/home/components/lessons/ForYouLessonCard.tsx
@@ -0,0 +1,89 @@
+import { useDimensions } from "@/hooks/use-dimensions";
+import { LessonCard } from "@/modules/lesson/components/LessonCard/LessonCard";
+import { clamp } from "@safe-fin/utils";
+import { useMemo } from "react";
+import type { CourseItem } from "@/modules/lesson/api";
+
+interface ForYouLessonCardProps {
+	course: CourseItem;
+}
+
+export const ForYouLessonCard = (props: ForYouLessonCardProps) => {
+	const { course } = props;
+	const { id, isSaved = 0, content, level, avgRating, rateCount } = course;
+
+	const { maxWidth = 350 } = useForYouLessonCardStyles({ horizontal: true });
+
+	return (
+		<LessonCard
+			id={id}
+			style={{
+				maxWidth,
+			}}
+		>
+			<LessonCard.Image source="https://ilarge.lisimg.com/image/28254022/1118full-iman-vellani.jpg">
+				<LessonCard.Bookmark isBookmarked={isSaved === 1} />
+			</LessonCard.Image>
+
+			<LessonCard.Body>
+				<LessonCard.Title>{content.title}</LessonCard.Title>
+				<LessonCard.Description>{content.shortDesc}</LessonCard.Description>
+
+				<LessonCard.Metadata
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+					}}
+				>
+					<LessonCard.MetadataLevel level={level} />
+					<LessonCard.Rating rating={avgRating} count={rateCount} />
+				</LessonCard.Metadata>
+			</LessonCard.Body>
+		</LessonCard>
+	);
+};
+
+interface ForYouLessonsCardLoading {
+	horizontal?: boolean;
+}
+
+const useForYouLessonCardStyles = (props: ForYouLessonsCardLoading) => {
+	const { width } = useDimensions("window");
+
+	const courseCardMaxWidth = useMemo(
+		() => clamp(300, Math.round(width * 0.85), 500),
+		[width],
+	);
+
+	if (!props.horizontal) return undefined;
+
+	return {
+		maxWidth: courseCardMaxWidth,
+		minWidth: courseCardMaxWidth,
+	};
+};
+
+ForYouLessonCard.Loading = (props: ForYouLessonsCardLoading) => {
+	const cardStyles = useForYouLessonCardStyles(props);
+
+	return (
+		<LessonCard.Loading style={cardStyles}>
+			<LessonCard.Image />
+
+			<LessonCard.Body>
+				<LessonCard.Title.Loading />
+				<LessonCard.Description.Loading />
+
+				<LessonCard.Metadata
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+					}}
+				>
+					<LessonCard.MetadataItem.Loading />
+					<LessonCard.MetadataItem.Loading />
+				</LessonCard.Metadata>
+			</LessonCard.Body>
+		</LessonCard.Loading>
+	);
+};
--- apps/app/app/modules/home/components/lessons/ForYouLessonsImpl.tsx
+++ apps/app/app/modules/home/components/lessons/ForYouLessonsImpl.tsx
@@ -1,19 +1,12 @@
-import { useMemo } from "react";
-import { BaseListItemSeparator, EmptyListView, ListView } from "@/components";
-import { useDimensions } from "@/hooks/use-dimensions";
-import { LessonCard } from "@/modules/lesson/components/LessonCard/LessonCard";
+import { $baseListItemSeparatorStyles, ListView } from "@/components";
 import { useGetForYouCourses } from "@/modules/lesson/hooks/api";
 import { getEmptyArr } from "@/pkg/ui";
-import { clamp } from "@/pkg/utils";
+import { useAppTheme } from "@/utils/useAppTheme";
+import { ForYouLessonCard } from "./ForYouLessonCard";
 
 export const ForYouLessonsImpl = () => {
 	const { courses } = useGetForYouCourses();
-	const { width } = useDimensions("window");
-
-	const courseCardMaxWidth = useMemo(
-		() => clamp(300, Math.round(width * 0.85), 500),
-		[width],
-	);
+	const { themed } = useAppTheme();
 
 	return (
 		<ListView
@@ -22,40 +15,8 @@ export const ForYouLessonsImpl = () => {
 			recycleItems
 			showsVerticalScrollIndicator={false}
 			keyExtractor={(item) => item.id.toString()}
-			ItemSeparatorComponent={BaseListItemSeparator}
-			ListEmptyComponent={EmptyListView}
-			renderItem={({ item }) => (
-				<LessonCard
-					id={item.id}
-					style={{
-						maxWidth: courseCardMaxWidth,
-					}}
-				>
-					<LessonCard.Image source="https://ilarge.lisimg.com/image/28254022/1118full-iman-vellani.jpg">
-						<LessonCard.Bookmark isBookmarked={item.isSaved === 1} />
-					</LessonCard.Image>
-
-					<LessonCard.Body>
-						<LessonCard.Title>{item.content.title}</LessonCard.Title>
-						<LessonCard.Description>
-							{item.content.shortDesc}
-						</LessonCard.Description>
-
-						<LessonCard.Metadata
-							style={{
-								display: "flex",
-								justifyContent: "space-between",
-							}}
-						>
-							<LessonCard.MetadataLevel level={item.level} />
-							<LessonCard.Rating
-								rating={item.avgRating}
-								count={item.rateCount}
-							/>
-						</LessonCard.Metadata>
-					</LessonCard.Body>
-				</LessonCard>
-			)}
+			contentContainerStyle={themed($baseListItemSeparatorStyles)}
+			renderItem={({ item }) => <ForYouLessonCard course={item} />}
 		/>
 	);
 };
@@ -66,19 +27,8 @@ interface ForYouLessonsLoading {
 
 ForYouLessonsImpl.Loading = (props: ForYouLessonsLoading) => {
 	const arr = getEmptyArr(3);
-	const { width } = useDimensions("window");
 
-	const courseCardMaxWidth = useMemo(
-		() => clamp(300, Math.round(width * 0.85), 500),
-		[width],
-	);
-
-	const cardStyles = props.horizontal
-		? {
-				maxWidth: courseCardMaxWidth,
-				minWidth: courseCardMaxWidth,
-			}
-		: undefined;
+	const { themed } = useAppTheme();
 
 	return (
 		<ListView
@@ -87,27 +37,8 @@ ForYouLessonsImpl.Loading = (props: ForYouLessonsLoading) => {
 			recycleItems
 			showsVerticalScrollIndicator={false}
 			keyExtractor={(index) => index.toString()}
-			ItemSeparatorComponent={BaseListItemSeparator}
-			renderItem={() => (
-				<LessonCard.Loading style={cardStyles}>
-					<LessonCard.Image />
-
-					<LessonCard.Body>
-						<LessonCard.Title.Loading />
-						<LessonCard.Description.Loading />
-
-						<LessonCard.Metadata
-							style={{
-								display: "flex",
-								justifyContent: "space-between",
-							}}
-						>
-							<LessonCard.MetadataItem.Loading />
-							<LessonCard.MetadataItem.Loading />
-						</LessonCard.Metadata>
-					</LessonCard.Body>
-				</LessonCard.Loading>
-			)}
+			contentContainerStyle={themed($baseListItemSeparatorStyles)}
+			renderItem={() => <ForYouLessonCard.Loading {...props} />}
 		/>
 	);
 };
