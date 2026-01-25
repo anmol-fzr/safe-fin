3d240e172572bb832d29b91187ec380ec0487cb8
Anmol <ainsa2279@gmail.com>
2026-01-25 22:18:24 +0530

    Remove: [DB] - Unused table and code

--- packages/db/dbml.ts
+++
@@ -1,7 +0,0 @@
-import { pgGenerate } from "drizzle-dbml-generator"; // Using Postgres for this example
-import * as schema from "./src/schema/index.ts";
-
-const out = "./schema.dbml";
-const relational = true;
-
-pgGenerate({ schema, out, relational });
