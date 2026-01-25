14bfbfec880198cbb80028635b508ebe4dffad96
Anmol <ainsa2279@gmail.com>
2026-01-25 22:24:10 +0530

    Fix: [SERVER] - Removed Old Storage router from v1 router

--- apps/server/src/api/v1/router.ts
+++ apps/server/src/api/v1/router.ts
@@ -1,13 +1,11 @@
 import { createTypedFactory } from "@/factory";
-import { authenticate, db } from "@/middleware";
 import { addressRouter } from "@/modules/address";
 import { authRouter } from "@/modules/auth";
 import { calculatorRouter } from "@/modules/calculator";
 import { lessonRouter } from "@/modules/lesson";
-import { savedRouter } from "@/modules/saved";
-import storageRouter from "@/modules/storage/storage.controller";
-
 import { profileRouter } from "@/modules/profile";
+import { savedRouter } from "@/modules/saved";
+
 //import { quizRouter } from "@/modules/quiz";
 
 const { createApp } = createTypedFactory();
@@ -20,7 +18,6 @@ v1Router
 	.route("/courses", lessonRouter)
 	.route("/calculator", calculatorRouter)
 	.route("/saved", savedRouter)
-	.route("/storage", storageRouter)
 	.route("/profile", profileRouter)
 	.route("/address", addressRouter);
 
