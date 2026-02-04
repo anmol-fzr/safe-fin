e8ac9911a87bf61a47a3df7800c3c25523a56835
Anmol <macmol@Anmols-MacBook-Air.local>
2026-02-04 23:35:41 +0530

    Fix: [UI] - Bug Fix make ellipsize use of maxLen for truncating text

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -3,7 +3,7 @@ const getEmptyArr = (length = 10) => Array.from({ length }, (_, i) => i);
 const ellipsize = (text: string, maxLen: number, appendText = " ...") => {
 	if (text.length < maxLen) return text;
 
-	return text.slice(0, 15) + appendText;
+	return text.slice(0, maxLen) + appendText;
 };
 
 export { getEmptyArr, ellipsize };
