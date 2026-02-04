ab338cb0fdcd1faee57837ef6941adab65c78b68
Anmol <macmol@Anmols-MacBook-Air.local>
2026-02-04 13:09:49 +0530

    Refactor: [APP] - Using same header

--- apps/app/app/routes/profile/_layout.tsx
+++ apps/app/app/routes/profile/_layout.tsx
@@ -1,25 +1,11 @@
-import { GoBack, ScreenHeader } from "@/components";
 import { Stack } from "expo-router";
 import { ProfileScreenHeader } from "../tabs/profile/_layout";
 
 export default function SavedLayout() {
 	return (
 		<Stack screenOptions={{ header: ProfileScreenHeader }}>
-			<Stack.Screen
-				name="public"
-				options={{
-					header: (props) => <GoBack tx="profileScreen:title" {...props} />,
-				}}
-			/>
-
-			<Stack.Screen
-				name="edit"
-				options={{
-					header: (props) => <GoBack tx="Public Profile" {...props} />,
-					presentation: "modal",
-				}}
-			/>
-
+			<Stack.Screen name="public" />
+			<Stack.Screen name="edit" />
 			<Stack.Screen name="demographics" />
 			<Stack.Screen name="financials" />
 			<Stack.Screen name="account" />
