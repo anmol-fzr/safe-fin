b937b9cd399b867c628f368e2d0a44f648e7a6c7
Anmol <macmol@Anmols-MacBook-Air.local>
2026-02-04 23:23:25 +0530

    Add: [UI] - Ellipsize Text utility fn

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,3 +1,9 @@
 const getEmptyArr = (length = 10) => Array.from({ length }, (_, i) => i);
 
-export { getEmptyArr };
+const ellipsize = (text: string, maxLen: number, appendText = " ...") => {
+	if (text.length < maxLen) return text;
+
+	return text.slice(0, 15) + appendText;
+};
+
+export { getEmptyArr, ellipsize };
