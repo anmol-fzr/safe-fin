4b97aab62b3edd51077677f4040332d6db0a297e
Anmol <macmol@Anmols-MacBook-Air.local>
2026-02-04 23:35:13 +0530

    Fix: [APP] - UI Changes in Public Profile Screen

--- apps/app/app/routes/profile/public.tsx
+++ apps/app/app/routes/profile/public.tsx
@@ -5,8 +5,13 @@ import { ActivityHeatmap } from "@/modules/profile/components/activity-heatmap";
 import { View } from "react-native";
 import { useAppTheme } from "@/utils/useAppTheme";
 import React, { ReactNode } from "react";
-import { FadeInDown, FadeInLeft, FadeInUp } from "react-native-reanimated";
+import Animated, {
+	FadeInDown,
+	FadeInLeft,
+	FadeInUp,
+} from "react-native-reanimated";
 import { useGetPublicProfile } from "@/modules/profile/hooks/queries";
+import { ellipsize } from "@safe-fin/ui/utils";
 
 export default function PublicProfileScreen() {
 	const { data } = useGetPublicProfile();
@@ -33,16 +38,63 @@ export default function PublicProfileScreen() {
 		},
 	];
 
+	const {
+		theme: { colors, spacing },
+	} = useAppTheme();
 	return (
-		<Screen preset="scroll" style={[$styles.container, { gap: 48 }]}>
-			<View style={{ gap: 24 }}>
-				<UserDetailsCard {...user} />
+		<Screen preset="scroll" style={[$styles.container, { gap: spacing.xxl }]}>
+			<View style={{ gap: spacing.lg }}>
+				<View
+					style={{
+						//flexDirection: "row",
+						//padding: spacing.sm,
+						//borderBottomWidth: StyleSheet.hairlineWidth,
+						borderBottomColor: colors.border,
+						//backgroundColor: "red",
+						alignItems: "center",
+						paddingTop: 0,
+					}}
+				>
+					<Animated.Image
+						entering={FadeInDown}
+						height={100}
+						width={100}
+						style={{
+							borderRadius: 100,
+						}}
+						source={{
+							uri: user.image,
+						}}
+					/>
+					<Text
+						size="xxl"
+						weight="medium"
+						entering={FadeInUp}
+						numberOfLines={1}
+					>
+						{ellipsize(user.name, 15)}
+					</Text>
+
+					{/*
+			<Link href="/profile/edit" asChild>
+				<PressableIcon
+					icon={Edit}
+					size={20}
+					style={{
+						position: "absolute",
+						top: 0,
+						right: 8,
+					}}
+				/>
+			</Link>
+      */}
+				</View>
 				<View
 					style={{
 						flexDirection: "row",
 						justifyContent: "space-evenly",
-						gap: spacing.xs,
-						margin: "auto",
+						//gap: spacing.xs,
+						//margin: "auto",
 						flexWrap: "wrap",
 					}}
 				>
@@ -77,7 +129,7 @@ const Streak = (props: StreakProps) => {
 		<View
 			style={{
 				minWidth: 150,
-				backgroundColor: colors.palette.neutral200,
+				//backgroundColor: colors.palette.neutral200,
 				borderRadius: roundness,
 				padding: spacing.sm,
 				alignItems: "center",
