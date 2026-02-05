40384b99eb97b8c82c2231afd6e31f673744f0d2
Anmol <macmol@Anmols-MacBook-Air.local>
2026-02-05 22:22:07 +0530

    Add: [APP] - Global Haptics to pressto

--- apps/app/app/components/Provider.tsx
+++ apps/app/app/components/Provider.tsx
@@ -2,6 +2,7 @@ import { AuthProvider, NotifierProvider } from "@safe-fin/ui/hooks";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import { PressablesConfig } from "pressto";
+import * as Haptics from "expo-haptics";
 import type { PropsWithChildren } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
@@ -37,6 +38,11 @@ export function Provider({ children }: PropsWithChildren) {
 									animationType="spring"
 									animationConfig={{ damping, stiffness }}
 									config={{ minScale: 0.95, activeOpacity: 0.1 }}
+									globalHandlers={{
+										onPress: () => {
+											Haptics.selectionAsync();
+										},
+									}}
 								>
 									<IconProvider>{children}</IconProvider>
 								</PressablesConfig>
