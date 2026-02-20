3be4c298835efe165efbfde015bda86c8d381999
Anmol <ainsa2279@gmail.com>
2026-02-21 00:06:48 +0530

    Fix: [APP] - No Cache using tanstack if in dev,

--- apps/app/app/utils/lib/query.ts
+++ apps/app/app/utils/lib/query.ts
@@ -1,12 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
+import { envs } from "../envs";
 
 export const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
 			refetchOnWindowFocus: false,
 			refetchOnMount: false,
-			staleTime: 0,
-			gcTime: 0,
+			staleTime: envs.isDev ? 0 : undefined,
+			gcTime: envs.isDev ? 0 : undefined,
 		},
 	},
 });
